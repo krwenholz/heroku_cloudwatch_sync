009e51d77d16467aa297730158daa509ad96a19a
Kyle R Wenholz <kyle@krwenholz.com>
2019-04-19 20:25:34 -0700

    More slimming and preparing for JSON logs

--- src/heroku_sync_to_cloudwatch.py
+++ src/heroku_sync_to_cloudwatch.py
@@ -5,22 +5,14 @@ Designed as a Python3.7 lambda.
 """
 
 import json
-import time
-import boto3
 import logging
+import time
 
-#  TODO(kyle): Remove use of this
-import iso8601
-
-# import requests
 from base64 import b64decode
-
-#  TODO(kyle): Remove use of this
-from pyparsing import Word, Suppress, nums, Optional, Regex, pyparsing_common, alphanums
-from syslog import LOG_DEBUG, LOG_WARNING, LOG_INFO, LOG_NOTICE
 from collections import defaultdict
+from syslog import LOG_DEBUG, LOG_INFO, LOG_NOTICE, LOG_WARNING
 
-#  TODO(kyle): Add basic check for password and username
+import boto3
 
 
 log = logging.getLogger("app.heroku.drain")
@@ -41,6 +33,7 @@ def lambda_handler(event, context):
 
 
 def handle_lambda_proxy_event(event):
+    print(event)
     body = event["body"]
     headers = event["headers"]
     logGroup = event["pathParameters"]["logGroup"]
@@ -53,62 +46,22 @@ def handle_lambda_proxy_event(event):
     assert headers["X-Forwarded-Proto"] == "https"
     assert headers["Content-Type"] == "application/logplex-1"
 
-    # split into chunks
-    def get_chunk(payload):
-        msg_len, syslog_msg_payload = payload.split(" ", maxsplit=1)
-        msg_len = int(msg_len)
-
-        # only grab msg_len bytes of syslog_msg
-        syslog_msg = syslog_msg_payload[0:msg_len]
-        next_payload = syslog_msg_payload[msg_len:]
-
-        yield syslog_msg
-
-        if next_payload:
-            yield from get_chunk(next_payload)
-
-    # group messages by source,app
-    # format for slack
-    srcapp_msgs = defaultdict(dict)
-    chunk_count = 0
-    for chunk in get_chunk(body):
-        chunk_count += 1
-        evt = parser.parse(chunk)
-
-        # if not filter_slack_msg(evt):
-        #    # skip stuff filtered out
-        #    continue
-
-        # add to group
-        sev = evt["severity"]
-        group_name = f"SEV:{sev} {evt['source']} {evt['appname']}"
-        if sev not in srcapp_msgs[group_name]:
-            srcapp_msgs[group_name][sev] = list()
-        body = evt["message"]
-        srcapp_msgs[group_name][sev].append(str(evt["timestamp"]) + ": " + evt["message"])
-
-    for group_name, sevs in srcapp_msgs.items():
-        cwl = boto3.client("logs")
-        for severity, lines in sevs.items():
-            if not lines:
-                continue
-            title = group_name
-            # format the syslog event as a slack message attachment
-            # slack_att = slack_format_attachment(log_msg=None, log_rec=evt)
-            # text = "\n" + "\n".join(lines)
-            # slack(text=text, title=title, attachments=[slack_att], channel=channel, severity=severity)
-            timestamp = int(round(time.time() * 1000))  # TODO: parse lines to set time to recorded time
-            # timestamp = evt["timestamp"] ....
-            for text in lines:
-                send_to_cloudwatch(cwl, logGroup, logStreamName, timestamp, text)
+    # for group_name, sevs in srcapp_msgs.items():
+    #    cwl = boto3.client("logs")
+    #    for severity, lines in sevs.items():
+    #        if not lines:
+    #            continue
+    #        title = group_name
+    #        timestamp = int(round(time.time() * 1000))  # TODO: parse lines to set time to recorded time
+    #        # timestamp = evt["timestamp"] ....
+    #        events = [{"timestamp": TIMESTAMP, "message": TODO} for foo in bar]
+    #        send_to_cloudwatch(cwl, logGroup, logStreamName, timestamp, events)
 
     # sanity-check number of parsed messages
     assert int(headers["Logplex-Msg-Count"]) == chunk_count
 
-    return ""
 
-
-def send_to_cloudwatch(cwl, logGroup, logGroupStream, timestamp, text):
+def send_to_cloudwatch(cwl, logGroup, logGroupStream, timestamp, events):
     stream_info = cwl.describe_log_streams(logGroupName=logGroup, logStreamNamePrefix=logGroupStream)["logStreams"][0]
     if "uploadSequenceToken" not in stream_info:
         cwl.put_log_events(
@@ -118,6 +71,6 @@ def send_to_cloudwatch(cwl, logGroup, logGroupStream, timestamp, text):
         cwl.put_log_events(
             logGroupName=logGroup,
             logStreamName=logGroupStream,
-            logEvents=[{"timestamp": timestamp, "message": text}],
+            logEvents=[events],
             sequenceToken=stream_info["uploadSequenceToken"],
         )
