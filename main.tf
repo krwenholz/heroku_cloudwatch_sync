74b46d089e255dcc35478a5f2a855381443ae04f
Kyle R Wenholz <kyle@krwenholz.com>
2019-04-25 19:17:21 -0700

    Working terraform for API Gateway

--- main.tf
+++ main.tf
@@ -11,45 +11,78 @@ variable region {
 # Lambda
 # --------------------------------------------------------------------
 
+data "archive_file" "zipped_lambda" {
+  output_path = "heroku_sync_to_cloudwatch.zip"
+  source_file = "${path.module}/src/heroku_sync_to_cloudwatch.py"
+  type        = "zip"
+}
+
 resource "aws_lambda_function" "this" {
   description      = "Drains Heroku logs into this account."
-  filename         = "src/heroku_sync_to_cloudwatch.py"
+  filename         = "heroku_sync_to_cloudwatch.zip"
   function_name    = "${var.function_name}"
-  handler          = "update_host_alarms.lambda_handler"
+  handler          = "heroku_sync_to_cloudwatch.lambda_handler"
   publish          = true
   role             = "${aws_iam_role.iam_role.arn}"
   runtime          = "python3.7"
-  source_code_hash = "${md5(file("src/heroku_sync_to_cloudwatch.py"))}"
+  source_code_hash = "${data.archive_file.zipped_lambda.output_base64sha256}"
   timeout          = "120"
+}
+
+resource "aws_lambda_permission" "post_session_trigger" {
+  statement_id  = "allowApiGatewayInvocation"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.this.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*"
 }
 
 # API Gateway
 # --------------------------------------------------------------------
 
-resource "aws_api_gateway_rest_api" "api" {
+resource "aws_api_gateway_rest_api" "this" {
   name = "${var.function_name}"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
-  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+resource "aws_api_gateway_resource" "logs" {
+  path_part   = "logs"
+  parent_id   = "${aws_api_gateway_rest_api.this.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
 }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
+resource "aws_api_gateway_resource" "log_group" {
+  path_part   = "{log_group}"
+  parent_id   = "${aws_api_gateway_resource.logs.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
+}
+
+# https://www.olicole.net/blog/2017/07/terraforming-aws-a-serverless-website-backend-part-3/
+resource "aws_api_gateway_resource" "log_stream" {
+  path_part   = "{log_stream}"
+  parent_id   = "${aws_api_gateway_resource.log_group.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
+}
+
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = "${aws_api_gateway_rest_api.this.id}"
+  resource_id   = "${aws_api_gateway_resource.log_stream.id}"
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
-  http_method             = "${aws_api_gateway_method.method.http_method}"
+  rest_api_id             = "${aws_api_gateway_rest_api.this.id}"
+  resource_id             = "${aws_api_gateway_resource.log_stream.id}"
+  http_method             = "${aws_api_gateway_method.post.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
+}
+
+resource "aws_api_gateway_deployment" "endpoint" {
+  depends_on = ["aws_api_gateway_method.post",  "aws_api_gateway_integration.integration"]
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
+  stage_name  = "default"
 }
 
 # IAM
@@ -100,6 +133,6 @@ resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment" {
 
 # Outputs
 ##############################################
-output "function_url" {
-  value = "https://${var.function_name}.execute-api.${var.region}.amazonaws.com"
+output "url" {
+  value = "${aws_api_gateway_deployment.endpoint.invoke_url}/"
 }
